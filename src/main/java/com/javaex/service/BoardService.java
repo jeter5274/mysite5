89bca305c4a7a47587ad310fe9ab46c6bac788c5
jeter5274 <jeter5274@gmail.com>
2021-02-01 09:47:00 +0900

    주석 수정

--- src/main/java/com/javaex/service/BoardService.java
+++ src/main/java/com/javaex/service/BoardService.java
@@ -34,15 +34,7 @@ public class BoardService {
 		
 		BoardVo post = boardDao.selectPost(no);
 
-		/* 본인의 글이 아니면 조회수 +1
-		 * 
-		 * 세션정보를 사용하기위해 Autowired 어노테이션을 쓰느냐, 
-		 * controller에서 매개변수로 받아 전달하는게 나은가?
-		 * 
-		 * controller에서는 client의 요청과 데이터를 전달하고 결과를 전달받아 뷰 응답하는 것이 역할이며,
-		 * 활용하지 않는 세션을 전달만하기 위해 불러올 필요가 없을 것 같음
-		 * 또한 글을 읽는 사람이 작성자인지 판단하는 것은 비즈니스 로직이라고 판단되어 Autowired를 활용 
-		 */
+		// 본인의 글이 아니면 조회수 +1
 		if(session.getAttribute("authUser") == null || post.getUserNo() != ((UserVo)session.getAttribute("authUser")).getNo()) {
 			boardDao.updateHit(no);			//DB hit +1
 			post.setHit(post.getHit()+1);	//이미 불러온 post hit+1
